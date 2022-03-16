1b1e9e4b464a8e3a7204db572a1bea070855cf75
leeceo97 <leeceo97@naver.com>
2022-03-16 18:19:18 +0900

    (fix)notification functions

--- notifications/models.py
+++ notifications/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete, m2m_changed
 from django.dispatch import receiver
 from accounts.models import User
+from posts.models import Post, Comment, Member
 
 
 class Notification(models.Model):
@@ -14,3 +15,16 @@ def create_user_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(user=instance, messages="가입을 축하합니다.")
 
+@receiver(post_save, sender=Comment)
+def create_comment_notification(sender, instance, created, **kwargs):
+    if created:
+        Notification.objects.create(user=instance.post.author, messages="새로운 댓글이 달렸습니다.")
+
+@receiver(post_save, sender=Member)
+def create_member_notification(sender, instance, created, **kwargs):
+    if created:
+        Notification.objects.create(user=instance.post.author, messages="새로운 가입신청이 왔습니다.")
+
+@receiver(post_delete, sender=Member)
+def delete_member_notification(sender, instance, **kwargs):
+    Notification.objects.create(user=instance.member, messages="가입이 거부당하셨습니다.")
